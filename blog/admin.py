7006716941860f41ac1ef0ae1b0848c70d3ab542
Shashi Essp <shashiessp@gmail.com>
2013-11-29 01:59:28 +0530

    updated models

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from blog.models import Category
+from blog.models import Category, Entry
 
 
 class CategoryAdmin(admin.ModelAdmin):
-	pass
+	prepopulated_fields = {'slug': ['title']}
+
+class EntryAdmin(admin.ModelAdmin):
+	prepopulated_fields = { 'slug': ['title']}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Entry, EntryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
+import datetime
 from django.db import models
 
 class Category(models.Model):
 	title = models.CharField(max_length=250)
-	slug  = models.SlugField(unique=True)
+	slug  = models.SlugField(unique_for_date='pub_date')
 	description = models.TextField()
 
-
+class Entry(models.Model):
+	title = models.CharField(max_length=250)
+	excerpt = models.TextField(blank=True)
+	body = models.TextField()
+	pub_date = models.DateTimeField(default=datetime.datetime.now)
 
 	def __unicode__(self):
 		return self.title
